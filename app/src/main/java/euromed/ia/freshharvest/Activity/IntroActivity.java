137a2c9ff2fa76798032bdde2ce44a0fe1051c73
medmac01 <th3justhacker@gmail.com>
2023-12-27 21:36:25 +0100

    forced light mode theme

--- app/src/main/java/euromed/ia/freshharvest/Activity/IntroActivity.java
+++ app/src/main/java/euromed/ia/freshharvest/Activity/IntroActivity.java
@@ -1,6 +1,7 @@
 package euromed.ia.freshharvest.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,6 +16,7 @@ public class IntroActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_intro);
 
         introBtn = findViewById(R.id.introBtn);
